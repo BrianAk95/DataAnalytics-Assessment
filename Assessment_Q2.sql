98de9628044789f3e0cfebbf5fe2681d5fed6c43
Brian Akpore <brianakpore95@gmail.com>
2025-05-17 17:51:44 +0100

    Task 2 done

---
+++ Assessment_Q2.sql
@@ -0,0 +1,35 @@
+-- Q2: Transaction Frequency Analysis
+-- Categorize customers by transaction frequency per month
+
+WITH txn_monthly AS (
+    SELECT
+        owner_id,
+        DATE_TRUNC('month', created_at) AS txn_month,
+        COUNT(id) AS monthly_txn
+    FROM savings_savingsaccount
+    GROUP BY owner_id, DATE_TRUNC('month', created_at)
+), -- subquery to calculate customers' transaction frequency per month
+customer_freq AS (
+    SELECT
+        owner_id,
+        AVG(monthly_txn) AS avg_txn_per_month
+    FROM txn_monthly
+    GROUP BY owner_id
+), -- subquery to calculate customers' average transactions frequency per month using the table txn_monthly
+freq_category AS (
+    SELECT
+        CASE
+            WHEN avg_txn_per_month >= 10 THEN 'High Frequency'
+            WHEN avg_txn_per_month BETWEEN 3 AND 9 THEN 'Medium Frequency'
+            ELSE 'Low Frequency'
+        END AS frequency_category,
+        owner_id,
+        avg_txn_per_month
+    FROM customer_freq
+) -- subquery to categorize customers by average transaction frequency per month using the table customer_freq
+SELECT
+    frequency_category,
+    COUNT(owner_id) AS customer_count,
+    ROUND((AVG(avg_txn_per_month)), 1) AS avg_transactions_per_month
+FROM freq_category
+GROUP BY frequency_category;
