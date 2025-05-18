7a932df9dc71b2cbf8acd564c42c610e0f874e73
Brian Akpore <brianakpore95@gmail.com>
2025-05-18 05:59:47 +0100

    Task 4 reviewed

--- Assessment_Q4.sql
+++ Assessment_Q4.sql
@@ -8,26 +8,30 @@ WITH txn_summary AS (
         COUNT(*) AS txn_count
     FROM savings_savingsaccount
     GROUP BY owner_id
-),
+), -- subquery to calculate the lifetime value and volume of transaction per customer
 tenure_data AS (
     SELECT
         id AS customer_id,
-        name,
-        DATE_PART('month', AGE(CURRENT_DATE, date_joined)) AS tenure_months
+        CONCAT(first_name, ' ', last_name) AS name,
+		TIMESTAMPDIFF(MONTH, date_joined, CURRENT_DATE) AS tenure_months
     FROM users_customuser
-),
+), -- subquery to calculate difference in month since become a customer till current date
 clv_calc AS (
     SELECT
-        t.customer_id,
-        t.name,
+        u.id as customer_id,
+        CONCAT(u.first_name, ' ', u.last_name) as name,
         td.tenure_months,
         ts.txn_count AS total_transactions,
-        ROUND((ts.total_txn * 0.001 / ts.txn_count), 2) AS avg_profit_per_transaction,
         ROUND((ts.txn_count / NULLIF(td.tenure_months, 0)) * 12 * (ts.total_txn * 0.001 / ts.txn_count), 2) AS estimated_clv
+-- This expression estimates customer lifetime value based on:
+-- Average monthly transaction activity - (ts.txn_count / NULLIF(td.tenure_months, 0))
+-- Assumed profit margin (0.1%) - (ts.total_txn * 0.001 / ts.txn_count)
+-- Projected over a year multiplyiing by 12
     FROM tenure_data td
     JOIN txn_summary ts ON td.customer_id = ts.owner_id
-    JOIN users_customuser t ON t.id = td.customer_id
+    JOIN users_customuser u ON u.id = td.customer_id
 )
 SELECT *
 FROM clv_calc
 ORDER BY estimated_clv DESC;
+
