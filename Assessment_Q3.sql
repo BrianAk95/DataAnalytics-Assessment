8ed2a9214d5dc2136dbb1d1d78e9fd025e349d8d
Brian Akpore <brianakpore95@gmail.com>
2025-05-18 09:44:05 +0100

    Task 3 done

--- Assessment_Q3.sql
+++ Assessment_Q3.sql
@@ -3,34 +3,37 @@
 
 WITH last_savings AS (
     SELECT
-        id AS plan_id,
-        owner_id,
+        p.id AS plan_id,
+        s.owner_id,
         'Savings' AS type,
-        MAX(created_at) AS last_transaction_date
-    FROM savings_savingsaccount
-    WHERE confirmed_amount > 0
-    GROUP BY id, owner_id
-),
+        MAX(s.transaction_date) AS last_transaction_date
+    FROM savings_savingsaccount s
+    left join plans_plan p on s.plan_id = p.id
+    WHERE p.is_regular_savings =1 and s.confirmed_amount > 0
+    GROUP BY p.id, s.owner_id
+), -- subquery for last transaction with respect to savings plan
 last_investments AS (
     SELECT
-        id AS plan_id,
-        owner_id,
+        p.id AS plan_id,
+        s.owner_id,
         'Investment' AS type,
-        MAX(created_at) AS last_transaction_date
-    FROM plans_plan
-    WHERE confirmed_amount > 0
-    GROUP BY id, owner_id
-),
-combined AS (
+        MAX(s.transaction_date) AS last_transaction_date
+    FROM savings_savingsaccount s
+    left join plans_plan p on s.plan_id = p.id
+    WHERE p.is_a_fund =1 and s.confirmed_amount > 0
+    GROUP BY p.id, s.owner_id
+), -- subquery for last transaction with respect to investment plan
+savings_investment AS (
     SELECT * FROM last_savings
     UNION ALL
     SELECT * FROM last_investments
 )
+-- Both savings and investment scenario unified in a table distinct by type
 SELECT
     plan_id,
     owner_id,
     type,
     last_transaction_date,
-    DATE_PART('day', CURRENT_DATE - last_transaction_date)::int AS inactivity_days
-FROM combined
-WHERE last_transaction_date <= CURRENT_DATE - INTERVAL '365 days';
+    TIMESTAMPDIFF(DAY, last_transaction_date, CURRENT_DATE) AS inactivity_days
+FROM savings_investment
+WHERE last_transaction_date <= CURRENT_DATE - INTERVAL 365 day; -- expected output
